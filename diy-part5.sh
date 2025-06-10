ce2741027523cb861f9946aadd5fe963cd4a049e
JohnnyDu0815 <39909450+JohnnyDu0815@users.noreply.github.com>
2025-06-11 04:15:22 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -19,6 +19,17 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
+# Remove the default apps
+sed -i 's/ddns-scripts_aliyun //g' include/target.mk
+sed -i 's/ddns-scripts_dnspod //g' include/target.mk
+sed -i 's/luci-app-ddns //g' include/target.mk
+sed -i 's/luci-app-vsftpd //g' include/target.mk
+sed -i 's/luci-app-“师夷长技以制夷”-plus //g' include/target.mk
+sed -i 's/luci-app-vlmcsd //g' include/target.mk
+sed -i 's/luci-app-accesscontrol //g' include/target.mk
+sed -i 's/luci-app-nlbwmon //g' include/target.mk
+sed -i 's/luci-app-wol //g' include/target.mk
+
 # 添加组播防火墙规则
 cat >> package/network/config/firewall/files/firewall.config <<EOF
 config rule
